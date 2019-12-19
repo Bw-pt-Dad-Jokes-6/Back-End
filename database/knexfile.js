02d71817f144b19a01591898ba223057e79e7fae
LiamColeE <liamedlinger@gmail.com>
2019-12-18 20:34:46 -0600

    setup everything

---
+++ database/knexfile.js
@@ -0,0 +1,11 @@
+// Update with your config settings.
+
+module.exports = {
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './dadjokes.db3' 
+    },
+    useNullAsdefault: true
+  }
+};
